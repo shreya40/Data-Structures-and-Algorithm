ed9fc9df01d8e1f89bcfe4e1583e3d877a319d67
shreya40 <88774355+shreya40@users.noreply.github.com>
2021-11-22 00:59:08 +0530

    Create 2.palindrome.java

---
+++ Love Babbar sheet/String/2.palindrome.java
@@ -0,0 +1,11 @@
+class Solution {
+    int isPalindrome(String S)
+    {
+       for(int i=0;i<S.length()/2;i++)
+       {
+         if( S.charAt(i)!=S.charAt(S.length()-i-1))
+         return 0;
+       }
+       return 1;
+    }
+};
